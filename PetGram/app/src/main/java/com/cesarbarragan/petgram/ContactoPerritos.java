edeaa862275f85f49bc57a00ffe0eb9e5bf81c3a
CABG01 <68351454+CABG01@users.noreply.github.com>
2020-08-04 15:44:12 -0500

    files

---
+++ PetGram/app/src/main/java/com/cesarbarragan/petgram/ContactoPerritos.java
@@ -0,0 +1,44 @@
+package com.cesarbarragan.petgram;
+
+public class ContactoPerritos {
+
+    private String nombrePerrito;
+    private int likes;
+    private int foto;
+
+
+    public ContactoPerritos(String nombrePerrito, int foto, int likes) {
+        this.nombrePerrito = nombrePerrito;
+        this.foto = foto;
+        this.likes = likes;
+    }
+
+    public String getNombrePerrito() {
+        return nombrePerrito;
+    }
+
+    public void setNombrePerrito(String nombrePerrito) {
+        this.nombrePerrito = nombrePerrito;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+
+
+
+
+}
